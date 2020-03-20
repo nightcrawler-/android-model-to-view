a8e4a221c42b471b17f0cf63b82dc7142579a67b
Frederick Nyawaya <o.frederickn@gmail.com>
2020-03-20 10:27:57 +0300

    Initial commit

---
+++ app/src/main/java/com/cafrecode/am2v/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cafrecode.am2v
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
