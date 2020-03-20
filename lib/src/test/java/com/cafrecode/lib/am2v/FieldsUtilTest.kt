31eb3f2fbbea7cd8e67a9a6e5b6b85dfadb00d4f
Frederick Nyawaya <o.frederickn@gmail.com>
2020-03-20 14:22:46 +0300

    0 for nulls, tests extra

--- lib/src/test/java/com/cafrecode/lib/am2v/FieldsUtilTest.kt
+++ lib/src/test/java/com/cafrecode/lib/am2v/FieldsUtilTest.kt
@@ -1,5 +1,6 @@
 package com.cafrecode.lib.am2v
 
+import android.graphics.DashPathEffect
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 
@@ -28,11 +29,23 @@ internal class FieldsUtilTest {
         assertEquals("Moisture:", packagingResult[1].title)
         assertEquals("67 %", packagingResult[1].content)
 
-        assertEquals("S 0:", packagingResult[3].title)
-        assertEquals("09 Kgs", packagingResult[3].content)
+        assertEquals("BBN:", packagingResult[3].title)
+        assertEquals("0", packagingResult[3].content)
 
-        assertEquals("S 1s:", packagingResult[4].title)
-        assertEquals("34 Kgs", packagingResult[4].content)
+        assertEquals("S 0:", packagingResult[4].title)
+        assertEquals("09 Kgs", packagingResult[4].content)
+
+        assertEquals("S 1s:", packagingResult[5].title)
+        assertEquals("34 Kgs", packagingResult[5].content)
+
+        val dashboard = Dashboard(null, null, 9.0, 10.2, 0.0, null)
+        val dashboardResult = FieldsUtil.getFieldValuePairs(dashboard)
+
+        assertEquals("Receiving:", dashboardResult[0].title)
+        assertEquals("0", dashboardResult[0].content)
+
+        assertEquals("Drying:", dashboardResult[2].title)
+        assertEquals("9.0", dashboardResult[2].content)
 
     }
 }
